1c07e3da7b7c16a8903a84168cca4929120d942b
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2021-09-09 10:23:04 +0530

    Directories are now relative

--- src/prompt.c
+++ src/prompt.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <pwd.h>
 
 #include "prompt.h"
 #include "utils.h"
 
-#define HOST_NAME_MAX 255
-#define USER_NAME_MAX 255
-
 char hostname[HOST_NAME_MAX];
 char *username;
+char *home;
+
+void format_directory(char **directory)
+{
+    home = getpwuid(getuid())->pw_dir;
+    int homelen = strlen(home);
+    if (strlen(*directory) < homelen)
+    {
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < homelen; i++)
+        {
+            if (home[i] != (*directory)[i])
+            {
+                return;
+            }
+        }
+    }
+    *(*directory + homelen - 1) = '~';
+    *directory = *directory + homelen - 1;
+}
 
 void prompt()
 {
@@ -19,5 +40,6 @@ void prompt()
     username = getpwuid(getuid())->pw_name;
 
     char *cwd = getcwd(NULL, 0);
+    format_directory(&cwd);
     printf(GREEN "[%s@%s " RESET "%s" GREEN "]$ " RESET, username, hostname, cwd);
 }
--- src/prompt.h
+++ src/prompt.h
@@ -1,6 +1,9 @@
 #ifndef PROMPT_H
 #define PROMPT_H
 
+#define HOST_NAME_MAX 255
+#define USER_NAME_MAX 255
+
 void prompt();
 
 #endif
